0aa583808b685bb53030245f6bd9241c18f4638e
RE093 <rupert.p@rollercoasterdigital.com>
2023-04-23 15:25:29 +0200

    RE-000: fixed some import issues, style the home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,29 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
-
 function App() {
-	const [count, setCount] = useState(0)
-
 	return (
 		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount(count => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
+			<h1 className="fs-900 fw-bold">Rup's Starter Template</h1>
+			<h2 className="margin-block-start-36 fs-600">Built using:</h2>
+			<ul className="margin-block-8">
+				<li className="margin-block-end-8">
+					The{" "}
+					<a
+						className="clr-neutral-light fw-bold"
+						href="https://vitejs.dev/guide/"
+					>
+						Vite create app with TS and React
+					</a>
+				</li>
+				<li>
+					Kevin Powells{" "}
+					<a
+						className="clr-neutral-light fw-bold"
+						href="https://www.beyondcss.dev/"
+					>
+						Beyond CSS
+					</a>{" "}
+					starter template
+				</li>
+			</ul>
 		</>
 	)
 }
